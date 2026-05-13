5b55d5d1685a629f05cedffc47d6c44e56f5d855
BrenEstefania <brendahs506@gmail.com>
2026-05-13 12:03:25 -0600

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors()); // 👈 ESTA LÍNEA ES CLAVE
+
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.json({ mensaje: "Hola mundo desde Render " });
+  res.json({ mensaje: "Hola mundo desde Render 🚀" });
 });
-
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor en puerto ${PORT}`);
